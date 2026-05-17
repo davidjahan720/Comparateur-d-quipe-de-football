367b7c039c35033636015a1bb9a628379db620ae
David Jahan <jahandavid@gmail.com>
2026-05-17 21:23:15 +0200

    fix: add real API Football IDs and logos for all 9 teams

--- src/store/watchlistStore.ts
+++ src/store/watchlistStore.ts
@@ -10,16 +10,16 @@ interface WatchlistStore {
 }
 
 const initialTeams: WatchlistTeam[] = [
-  { id: 0, name: "QPR", nameOfficial: "Queens Park Rangers FC", logo: "", country: "Angleterre", league: "EFL Championship" },
-  { id: 0, name: "Brighton", nameOfficial: "Brighton & Hove Albion FC", logo: "", country: "Angleterre", league: "Premier League" },
-  { id: 0, name: "Aston Villa", nameOfficial: "Aston Villa FC", logo: "", country: "Angleterre", league: "Premier League" },
-  { id: 0, name: "Eibar", nameOfficial: "SD Eibar", logo: "", country: "Espagne", league: "La Liga 2" },
-  { id: 0, name: "Malaga", nameOfficial: "Málaga CF", logo: "", country: "Espagne", league: "La Liga Hypermotion" },
-  { id: 0, name: "Coventry", nameOfficial: "Coventry City FC", logo: "", country: "Angleterre", league: "EFL Championship" },
-  { id: 0, name: "Lorient", nameOfficial: "FC Lorient", logo: "", country: "France", league: "Ligue 2" },
-  { id: 0, name: "Brentford", nameOfficial: "Brentford FC", logo: "", country: "Angleterre", league: "Premier League" },
-  { id: 0, name: "Villarreal", nameOfficial: "Villarreal CF", logo: "", country: "Espagne", league: "La Liga" },
-].map(t => ({ ...t, needsIdResolution: true }))
+  { id: 72, name: "QPR", nameOfficial: "Queens Park Rangers FC", logo: "https://media.api-sports.io/football/teams/72.png", country: "Angleterre", league: "EFL Championship" },
+  { id: 51, name: "Brighton", nameOfficial: "Brighton & Hove Albion FC", logo: "https://media.api-sports.io/football/teams/51.png", country: "Angleterre", league: "Premier League" },
+  { id: 66, name: "Aston Villa", nameOfficial: "Aston Villa FC", logo: "https://media.api-sports.io/football/teams/66.png", country: "Angleterre", league: "Premier League" },
+  { id: 545, name: "Eibar", nameOfficial: "SD Eibar", logo: "https://media.api-sports.io/football/teams/545.png", country: "Espagne", league: "La Liga 2" },
+  { id: 535, name: "Málaga", nameOfficial: "Málaga CF", logo: "https://media.api-sports.io/football/teams/535.png", country: "Espagne", league: "La Liga Hypermotion" },
+  { id: 1346, name: "Coventry", nameOfficial: "Coventry City FC", logo: "https://media.api-sports.io/football/teams/1346.png", country: "Angleterre", league: "EFL Championship" },
+  { id: 97, name: "Lorient", nameOfficial: "FC Lorient", logo: "https://media.api-sports.io/football/teams/97.png", country: "France", league: "Ligue 2" },
+  { id: 55, name: "Brentford", nameOfficial: "Brentford FC", logo: "https://media.api-sports.io/football/teams/55.png", country: "Angleterre", league: "Premier League" },
+  { id: 533, name: "Villarreal", nameOfficial: "Villarreal CF", logo: "https://media.api-sports.io/football/teams/533.png", country: "Espagne", league: "La Liga" },
+].map(t => ({ ...t, needsIdResolution: false }))
 
 export const useWatchlistStore = create<WatchlistStore>()(
   persist(
